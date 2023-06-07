896c74e9238bec316b72027f4e9457e072dc83e0
agyeievans <evanskofiagyei06@gmail.com>
2023-06-07 11:55:21 +0000

    created model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class CountryData(models.Model):
+    country = models.CharField(max_length=100)
+    population = models.IntegerField()
+
+    # string representation of the model
+    def __str__(self):
+        return f'{self.country} - {self.population}'
+    
+    class Meta:
+        verbose_name_plural = 'Country Population Data'
+
