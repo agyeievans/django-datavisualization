95cc24dd33476faaf0b13b68470098e90f573c2d
agyeievans <evanskofiagyei06@gmail.com>
2023-06-07 12:31:20 +0000

    add data to db

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .models import CountryData
+from .forms import CountryDataForm
 
 # Create your views here.
 def index(request):
     data = CountryData.objects.all()
+    if request.method == 'POST':
+        form = CountryDataForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = CountryDataForm()
     context = {
         'data': data,
+        'form': form,
     }
     return render(request, 'dashboard/index.html', context)
+
